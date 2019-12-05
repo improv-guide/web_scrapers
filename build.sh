0a72fceec2f493b2e33a942e4069f19e0e44905a
sal <slpg500a>
2019-12-05 23:10:43 +0000

    Switch to Python 3

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 . ./.env
 echo "Building ${IMPROV_GUIDE_SCRAPERS_VERSION}"
-echo ${IMPROV_GUIDE_SCRAPERS_VERSION} > ./src/version.txt
+echo "${IMPROV_GUIDE_SCRAPERS_VERSION}" > ./src/version.txt
 docker-compose -f docker-compose.yaml build --force
